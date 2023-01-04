81937c8cf47ccdc1a3f82aac4ed460179e6d1a2e
DESKTOP-S35E8BD\kys <kys@greenit.cc>
2023-01-04 21:41:43 +0900

    first commit

---
+++ src/main/java/com/example/projectJae/ProjectJaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectJae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectJae/ProjectJaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectJae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
